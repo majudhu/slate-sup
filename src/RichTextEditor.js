8d4f2ebc7e49ec67f935a1f984972a0e2b2dd1ad
majudhu <majudhu.sup@gmail.com>
2021-10-20 22:01:51 +0500

    create rich text editor

--- src/RichTextEditor.js
+++ src/RichTextEditor.js
@@ -1,13 +1,151 @@
-import { useMemo } from "react";
-import { createEditor } from "slate";
-import { Editable, Slate, withReact } from "slate-react";
+import { IconButton } from "@material-ui/core";
+import { useCallback, useMemo } from "react";
+import {
+  MdFormatBold,
+  MdFormatItalic,
+  MdFormatListBulleted,
+  MdFormatListNumbered,
+  MdFormatQuote,
+  MdFormatUnderlined,
+  MdLooksOne,
+  MdLooksTwo,
+} from "react-icons/md";
+import {
+  createEditor,
+  Editor,
+  Element as SlateElement,
+  Transforms,
+} from "slate";
+import { withHistory } from "slate-history";
+import { Editable, Slate, useSlate, withReact } from "slate-react";
+const LIST_TYPES = ["numbered-list", "bulleted-list"];
 
 export default function RichTextEditor({ value, setValue }) {
-  const editor = useMemo(() => withReact(createEditor()), []);
+  const editor = useMemo(() => withReact(withHistory(createEditor())), []);
+  const renderElement = useCallback((props) => <Element {...props} />, []);
+  const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
 
   return (
     <Slate editor={editor} value={value} onChange={setValue}>
-      <Editable />
+      <div className="d-flex align-items-center">
+        <MarkButton format="bold" icon={<MdFormatBold />} />
+        <MarkButton format="italic" icon={<MdFormatItalic />} />
+        <MarkButton format="underline" icon={<MdFormatUnderlined />} />
+        <BlockButton format="heading-one" icon={<MdLooksOne />} />
+        <BlockButton format="heading-two" icon={<MdLooksTwo />} />
+        <BlockButton format="block-quote" icon={<MdFormatQuote />} />
+        <BlockButton format="numbered-list" icon={<MdFormatListNumbered />} />
+        <BlockButton format="bulleted-list" icon={<MdFormatListBulleted />} />
+      </div>
+      <Editable renderElement={renderElement} renderLeaf={renderLeaf} />
     </Slate>
+  );
+}
+
+function MarkButton({ format, icon }) {
+  const editor = useSlate();
+  const isMarked = Editor.marks(editor)?.[format];
+  return (
+    <IconButton
+      size="small"
+      className={isMarked ? "text-dark" : "text-muted"}
+      onClick={() =>
+        isMarked
+          ? Editor.removeMark(editor, format)
+          : Editor.addMark(editor, format, true)
+      }
+    >
+      {icon}
+    </IconButton>
+  );
+}
+
+const toggleBlock = (editor, format) => {
+  const isActive = isBlockActive(editor, format);
+  const isList = LIST_TYPES.includes(format);
+
+  Transforms.unwrapNodes(editor, {
+    match: (n) =>
+      !Editor.isEditor(n) &&
+      SlateElement.isElement(n) &&
+      LIST_TYPES.includes(n.type),
+    split: true,
+  });
+  const newProperties = {
+    type: isActive ? "paragraph" : isList ? "list-item" : format,
+  };
+  Transforms.setNodes(editor, newProperties);
+
+  if (!isActive && isList) {
+    const block = { type: format, children: [] };
+    Transforms.wrapNodes(editor, block);
+  }
+};
+
+const isBlockActive = (editor, format) => {
+  if (!editor.selection) return false;
+
+  const [match] = Editor.nodes(editor, {
+    at: Editor.unhangRange(editor, editor.selection),
+    match: (n) =>
+      !Editor.isEditor(n) && SlateElement.isElement(n) && n.type === format,
+  });
+
+  return !!match;
+};
+
+const Element = ({ attributes, children, element }) => {
+  switch (element.type) {
+    case "block-quote":
+      return <blockquote {...attributes}>{children}</blockquote>;
+    case "bulleted-list":
+      return (
+        <ul className="list-disc" {...attributes}>
+          {children}
+        </ul>
+      );
+    case "heading-one":
+      return <h1 {...attributes}>{children}</h1>;
+    case "heading-two":
+      return <h2 {...attributes}>{children}</h2>;
+    case "list-item":
+      return <li {...attributes}>{children}</li>;
+    case "numbered-list":
+      return (
+        <ol className="list-decimal" {...attributes}>
+          {children}
+        </ol>
+      );
+    default:
+      return <p {...attributes}>{children}</p>;
+  }
+};
+
+const Leaf = ({ attributes, children, leaf }) => {
+  if (leaf.bold) {
+    children = <strong>{children}</strong>;
+  }
+  if (leaf.italic) {
+    children = <em>{children}</em>;
+  }
+  if (leaf.underline) {
+    children = <u>{children}</u>;
+  }
+  return <span {...attributes}>{children}</span>;
+};
+
+function BlockButton({ format, icon }) {
+  const editor = useSlate();
+  return (
+    <IconButton
+      size="small"
+      className={isBlockActive(editor, format) ? "text-dark" : "text-muted"}
+      onClick={(event) => {
+        event.preventDefault();
+        toggleBlock(editor, format);
+      }}
+    >
+      {icon}
+    </IconButton>
   );
 }
